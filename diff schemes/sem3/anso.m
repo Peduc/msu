6c6c44a4f7040d40da8b9422fc3116c4be44fa8c
Peduc <zork353@gmail.com>
2019-01-21 11:40:07 +0300

    Deleted odd files

--- diff schemes/sem3/anso.m
+++
@@ -1,3 +0,0 @@
-function anso = anso( T,X )
-    anso=pi-X+0.5*cos(X/2)*(exp(T) - exp(-T));
-end
--- diff schemes/sem4/ansol4.m
+++
@@ -1,6 +0,0 @@
-function ansol4 = Analyt( T,X )
-a=1/2;
-ansol4=cos(T)+cos(3*a*T)*sin(3*X);
-% cos(X/2)*exp(-2*T)*(8/pi()+(1/3)*(exp(3*T)-1));
-end
-
